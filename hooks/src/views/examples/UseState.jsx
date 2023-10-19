580f0c306333ef1e9d321c9798d0b24d680d4fe3
Modena <felipe.kroitor@gmail.com>
2023-10-19 16:34:56 -0300

    feat: first exercise

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
-
+import React from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import { useState } from "react";
+import SectionTitle from "../../components/layout/SectionTitle";
 const UseState = (props) => {
-    return (
-        <div className="UseState">
-            <PageTitle
-                title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
-        </div>
-    )
-}
+  const [counter, setCounter] = useState(0);
 
-export default UseState
+  function raiseCounter() {
+    setCounter(counter + 1);
+  }
+
+  function decreaseCounter() {
+    setCounter(counter - 1);
+  }
+  return (
+    <div className="UseState">
+      <PageTitle
+        title="Hook UseState"
+        subtitle="Estado em componentes funcionais!"
+      />
+      <div>
+        <SectionTitle title="Ex. 1" />
+        <div>
+          <p>
+            {" "}
+            Valor do contador: <span className="text"> {counter}</span>
+          </p>
+        </div>
+        <button onClick={raiseCounter}>Aumentar contador</button>
+        <button onClick={decreaseCounter}>Diminui contador</button>
+      </div>
+    </div>
+  );
+};
+
+export default UseState;
